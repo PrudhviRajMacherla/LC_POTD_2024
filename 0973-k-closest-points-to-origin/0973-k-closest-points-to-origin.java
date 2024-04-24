4f8f70b0d1a1f56cb5fc3995836d0629b8ac7ebe
PrudhviRajMacherla <106004692+PrudhviRajMacherla@users.noreply.github.com>
2024-04-24 23:48:17 +0530

    Time: 37 ms (30.21%), Space: 51 MB (67.38%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
@@ -1,5 +1,5 @@
 class Solution {
-    public class tuple implements Comparable<tuple>{
+    public class tuple {
         int x;
         int y;
         public tuple(int x,int y)
@@ -7,27 +7,37 @@ class Solution {
             this.x=x;
             this.y=y;
         }
-        public int compareTo(tuple that)
-        {
-            double d1 = Math.sqrt((this.x*this.x)+(this.y*this.y));
-            double d2=  Math.sqrt((that.x*that.x)+(that.y*that.y));
-            if(d1<d2)
-                return -1;
-            return 1;
-        }
+        
     }
     public int[][] kClosest(int[][] points, int k) 
     {
-        PriorityQueue<tuple> pq = new PriorityQueue<tuple>();
+        List<tuple> al = new ArrayList<>();
         for(int i = 0;i<points.length;i++)
         {
-            pq.add(new tuple(points[i][0],points[i][1]));
+            al.add(new tuple(points[i][0],points[i][1]));
         }
+        Comparator<tuple> com = new Comparator<tuple>()
+        {
+            public int compare(tuple t1,tuple t2)
+            {
+                int x1=t1.x*t1.x;
+                int y1=t1.y*t1.y;
+                double dis1 = Math.sqrt(x1+y1);
+                int x2=t2.x*t2.x;
+                int y2=t2.y*t2.y;
+                double dis2 = Math.sqrt(x2+y2);
+                if(dis1<dis2)
+                    return -1;
+                return 1;
+                
+            }
+        };
+        Collections.sort(al,com);
         int[][] ans = new int[k][2];
         for(int i = 0;i<k;i++)
         {
-            tuple temp = pq.peek();
-            pq.remove();
+            tuple temp = al.get(i);
+            //.remove();
             ans[i][0]=temp.x;
             ans[i][1]=temp.y;
         }
